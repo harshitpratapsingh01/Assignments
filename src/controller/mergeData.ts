933a4fcb5ad6867b7d97726c64570458593ff6d6
harshitpratapsingh01 <harshit.singh@appinventiv.com>
2023-07-08 18:07:08 +0530

    Modify merge api

--- src/controller/mergeData.ts
+++ src/controller/mergeData.ts
@@ -3,33 +3,19 @@ import { newfilepath } from '../routes/handlefiles';
 
 const MergeData = (file1Path,file2Path,req,res) => {
     fs.readFile(file1Path, (err, data) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            fs.writeFile(newfilepath, data, (err) => {
-                if (err) {
-                    console.log(err);
-                }
-            });
-            fs.readFile(file2Path, (err, data) => {
-                if (err) {
-                    console.log(err);
-                }
-                else {
-                    fs.appendFile(newfilepath, ` ${data}`, (err) => {
-                        if (err) {
-                            console.log(err);
-                        }
-                    });
-
-                }
-                console.log("new file created");
-                res.send("new file created");
-            })
-
-        }
-    })
+        if (err) throw err;
+        fs.writeFile(newfilepath, data, (err) => {
+            if (err) throw err;
+        });
+    });
+    fs.readFile(file2Path, (err, data) => {
+        if (err) throw err;
+        fs.appendFile(newfilepath, ` ${data}`, (err) => {
+            if (err) throw err;
+        });
+    });    
+    console.log("new file created");
+    res.send("new file created");
 }
 
 export {MergeData};
